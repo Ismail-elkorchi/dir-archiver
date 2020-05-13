e0aa04f9260f718d703ca3f65dde6c86448bd66a
Ismail El Korchi <ismail.elkorchi@gmail.com>
2020-05-13 04:18:25 +0000

    Use double dash options

--- cli.js
+++ cli.js
@@ -8,16 +8,16 @@ var zipPath = '';
 var excludes = [];
 
 for ( argumentIndex in arguments ) {
-    if( arguments[argumentIndex] === '-src' ) {
+    if( arguments[argumentIndex] === '--src' ) {
         directoryPath = arguments[parseInt(argumentIndex) + 1];
     }
-    if( arguments[argumentIndex] === '-dest' ) {
+    if( arguments[argumentIndex] === '--dest' ) {
         zipPath = arguments[parseInt(argumentIndex) + 1];
     }
     if( afterExclude === true ) {
         excludes.push( arguments[argumentIndex] );
     }
-    if( arguments[argumentIndex] === '-exclude' ) {
+    if( arguments[argumentIndex] === '--exclude' ) {
         var afterExclude = true;
     }
 }
