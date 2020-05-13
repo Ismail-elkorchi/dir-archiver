6fb4b4a7564c6dff90ab626829596cae5bd7cd82
Ismail El Korchi <ismail.elkorchi@gmail.com>
2020-05-13 03:41:02 +0000

    Add cli script

---
+++ cli.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var DirArchiver = require('./index');
+
+const arguments = process.argv;
+var directoryPath = '';
+var zipPath = '';
+var excludes = [];
+
+for ( argumentIndex in arguments ) {
+    if( arguments[argumentIndex] === '-src' ) {
+        directoryPath = arguments[parseInt(argumentIndex) + 1];
+    }
+    if( arguments[argumentIndex] === '-dest' ) {
+        zipPath = arguments[parseInt(argumentIndex) + 1];
+    }
+    if( afterExclude === true ) {
+        excludes.push( arguments[argumentIndex] );
+    }
+    if( arguments[argumentIndex] === '-exclude' ) {
+        var afterExclude = true;
+    }
+}
+
+const archive = new DirArchiver(directoryPath, zipPath, excludes);
+archive.createZip();
